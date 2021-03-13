9363f238a718ff2588abd359666014a3afbc0d1f
Luis Torres Cereño <torrescereno@gmail.com>
2021-03-12 22:37:27 -0300

    Desafio 2 - Correccion agregar componente navbar en app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 
+/* Componets */
+import NavBar from './components/NavBar'
+
 function App() {
   return (
     <div className="App">
+      <NavBar />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-/* Componets */
-import NavBar from './components/NavBar'
-
 ReactDOM.render(
   <React.StrictMode>
-    <NavBar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
